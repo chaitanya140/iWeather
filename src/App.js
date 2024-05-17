4c817466a064e0621da536bf902ef049aea0e244
Chaitanya Deshpande <110080940+chaitanya140@users.noreply.github.com>
2024-05-17 16:04:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Weather from './Components/iWeather';
+import bgn from './bgn.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
